90f372eda737c58d877b42e5010a4b07abb67d12
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-07-04 16:01:44 +0200

    feat: proton vicinae extensions

---
+++ packages/proton-pass-vicinae-extension.nix
@@ -0,0 +1,12 @@
+{
+  mkRayCastExtension,
+  sources,
+  ...
+}:
+
+(mkRayCastExtension {
+  version = "0.1.0";
+  name = "proton-pass-vicinae-extension";
+
+  src = sources.protonPassRaycastExtension;
+})
