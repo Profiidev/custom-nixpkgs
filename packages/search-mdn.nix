c739b3f389288561a643cb2fa42287c7daf699bd
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-07-06 16:39:17 +0200

    fix: search mdn src cache

--- packages/search-mdn.nix
+++ packages/search-mdn.nix
@@ -1,6 +1,5 @@
 {
   mkRayCastExtension,
-  applyPatches,
   sources,
   ...
 }:
@@ -9,13 +8,5 @@ mkRayCastExtension {
   version = "0.1.0";
   name = "search-mdn";
 
-  src = applyPatches {
-    name = "search-mdn-src";
-    src = sources.searchMdnRaycastExtension;
-    patches = [ ../patches/search-mdn.patch ];
-
-    outputHashMode = "recursive";
-    outputHashAlgo = "sha256";
-    outputHash = "sha256-aTtVzjAQ78k8fR6xc1i6jxXY35ZtMfTStnKj/Heh6/k=";
-  };
+  src = sources.searchMdnRaycastExtension;
 }
